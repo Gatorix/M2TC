23aa360785f51431581a090ca6d0ad0e1fdf9ab8
gatorix <caosheng0000@outlook.com>
2020-12-12 00:08:33 +0800

    m

--- parsing.py
+++ parsing.py
@@ -47,6 +47,7 @@ root_topic = tc_dict['rootTopic']
 
 # print(collect(root_topic))
 
+retlist = []
 
 
 def get_target_value(key, dic, tmp_list):
@@ -60,12 +61,13 @@ def get_target_value(key, dic, tmp_list):
         return 'argv[1] not an dict or argv[-1] not an list '
 
     if key in dic.keys():
-        tmp_list.append(dic[key])  # 传入数据存在则存入tmp_list
 
+        tmp_list.append(dic[key])  # 传入数据存在则存入tmp_list
 
     for value in dic.values():  # 传入数据不符合则对其value值进行遍历
         if isinstance(value, dict):
             get_target_value(key, value, tmp_list)  # 传入数据的value值是字典，则直接调用自身
+
         elif isinstance(value, (list, tuple)):
             # 传入数据的value值是列表或者元组，则调用_get_value
             _get_value(key, value, tmp_list)
@@ -82,6 +84,19 @@ def _get_value(key, val, tmp_list):
             _get_value(key, val_, tmp_list)   # 传入数据的value值是列表或者元组，则调用自身
 
 
-print(get_target_value('title', root_topic, []))
 
+def print_dict(v, prefix='',temp_list=[]):
+    if isinstance(v, dict):
+        for k, v2 in v.items():
+            p2 = "{}['{}']".format(prefix, k)
+            print_dict(v2, p2)
+    elif isinstance(v, list):
+        for i, v2 in enumerate(v):
+            p2 = "{}[{}]".format(prefix, i)
+            print_dict(v2, p2)
+    else:
+        print('{}[{}]'.format(prefix, repr(v)))
+        return temp_list
 
+print_dict(root_topic, prefix='')
+# print(pd)
